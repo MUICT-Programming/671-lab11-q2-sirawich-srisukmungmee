e22ef3539e37d5984eb08f293814da024186ad39
sirawich-srisukmungmee <sirawich.srs@student.mahidol.edu>
2024-10-20 19:42:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# YOUR CODE HERE
+n=int(input())
+lst1=[]
+lst2=[]
+updated_list=[]
+def summation(n):
+    for i in range(n):
+        list1=int(input())
+        lst1.append(list1)
+    for i in range(n):
+        list2=int(input())
+        lst2.append(list2)
+        updated_list.append(lst1[i]+lst2[i])
+    return updated_list
+def find_min_max(updated_list):
+    min_value=min(updated_list)
+    max_value=max(updated_list)
+print(summation(n))
+print(find_min_max(updated_list))
